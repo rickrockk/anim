64121e0342d356c53cb546795b8d9c94aeed9f0e
Кирилл <mrkillraider@yandex.ru>
2024-05-01 20:57:37 +0300

    another small layout fix

--- anim_front/src/views/Profile/ProfileHeader/ProfileHeader.tsx
+++ anim_front/src/views/Profile/ProfileHeader/ProfileHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ProfileHeader.scss'
 import Cover from '/src/assets/cover.jpg'
+import Right from '/src/assets/Right.svg'
 import ProfileImg from '/src/assets/author_img.png'
 
 export function ProfileHeader() {
@@ -8,6 +9,7 @@ export function ProfileHeader() {
         <div className="main__profile-header">
             <div className="main__cover">
                 <p className="profile__name">Алина Матросова</p>
+                <button className="profile__cover-change">Изменить <span className="btn__arrow"><img src={Right} alt="Стрелка вправо"/></span></button>
             </div>
             <div className="main__profile-stats">
                 <img className="profile__img" src={ProfileImg} alt="Аватарка" className="profile__img"/>
