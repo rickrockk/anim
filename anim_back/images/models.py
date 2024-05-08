ebdf688d746680e3295b30bf64cf75c88d405bda
akobyansamvel <s.o.akobian@stud.mospolytech.ru>
2024-05-08 18:23:33 +0300

    add methods for photo

--- anim_back/images/models.py
+++ anim_back/images/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from authentication.models import User
+from django.utils import timezone
+
 
 class Image(models.Model):
-    name = models.CharField(verbose_name="название",max_length=255,default='SOME STRING')
-    photo = models.ImageField(verbose_name='изображения',upload_to='images/photos')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', null=True)
+    photo = models.ImageField(verbose_name='Изображение', upload_to='images/photos')
 
-    def __self__(self):
+    def __str__(self):
         return self.name
-    
+
     class Meta:
-        verbose_name='Фото'
-        verbose_name_plural='Фото'
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
--- anim_back/images/views.py
+++ anim_back/images/views.py
@@ -1,7 +1,36 @@
-from rest_framework.viewsets import ModelViewSet
-from images.serializers import ImageSerializer
-from images.models import Image
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from .models import Image
+from .serializers import ImageSerializer
 
-class ImageViewSet(ModelViewSet):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user and str(obj.id) == str(view.kwargs.get(view.lookup_field))
+
+class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    lookup_field = 'id'
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
+
+    def delete(self, request, *args, **kwargs):
+        self.get_object()
+        self.check_object_permissions(request, self.get_object()) 
+        self.perform_destroy(self.get_object())  
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user) 
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_permissions(self):
+
+        if self.action == 'destroy':
+            return [IsOwnerOrReadOnly()]
+        return super().get_permissions()
